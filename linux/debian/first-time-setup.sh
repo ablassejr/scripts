9ceee4671488f9b248e232b148dcfd08a603860e
Ablasse Kingcaid-Ouedraogo <158793596+ablassejr@users.noreply.github.com>
2025-11-01 17:02:35 -0500

    .

--- linux/debian/first-time-setup.sh
+++ linux/debian/first-time-setup.sh
@@ -1,11 +1,15 @@
-sudo apt-get update
-sudo apt upgrade -y
-sudo apt install git gh curl make
+apt-get update
+apt upgrade
+for package in git gh curl make; do
+  apt install -y $package
+done
+apt install -y git
+gh curl make
 git clone https://github.com/asdf-vm/asdf.git --branch v0.18.0 && cd asdf && make && cp ./bin/asdf /usr/local/bin/
-echo "export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"" >> ~/.bash_profile
-echo "source ~/.bash_profile" >> ~/.bashrc
+echo "export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"" >>~/.bash_profile
+echo "source ~/.bash_profile" >>~/.bashrc
 source "$HOME/.bashrc"
-for url in  "https://github.com/asdf-vm/asdf-nodejs.git" "https://github.com/asdf-vm/asdf-ruby.git" "https://github.com/asdf-community/asdf-python" "https://github.com/asdf-community/asdf-rust.git"; do
+for url in "https://github.com/asdf-vm/asdf-nodejs.git" "https://github.com/asdf-vm/asdf-ruby.git" "https://github.com/asdf-community/asdf-python" "https://github.com/asdf-community/asdf-rust.git"; do
   plugin=${url##*asdf-}
   asdf plugin add $plugin $url
   asdf install $plugin latest
